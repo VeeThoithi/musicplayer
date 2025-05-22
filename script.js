784a0e3dfc871952ebc1b2ee9055858604231071
VeeThoithi <thoithivenessa33@gmail.com>
2025-05-22 15:19:02 +0300

    Added the songs and the scripts to access them

--- script.js
+++ script.js
@@ -10,14 +10,38 @@ const songs =[
     {
         "title" : "Teenage Fever",
         "artist" : "Drake",
-        "file"  : "media/songs/Teenage Fever.mp3",
+        "file"  : "media/Songs/Teenage Fever.mp3",
         "songimage" : "media/drake 2.jpg"
     },
     {
-        "title" : "Teenage Fever",
-        "artist" : "Drake",
-        "file"  : "media/songs/Teenage Fever.mp3",
-        "songimage" : "media/drake 2.jpg"
+        "title" : "Leave",
+        "artist" : "Cil",
+        "file"  : "media/Songs/Leave.mp3",
+        "songimage" : "media/Cil Album cover.jpg"
+    },
+    {
+        "title" : "One More Shot",
+        "artist" : "Cil",
+        "file"  : "media/songs/One More Shot.mp3",
+        "songimage" : "media/Cil Album cover.jpg"
+    },
+    {
+        "title" : "Try",
+        "artist" : "Cil",
+        "file"  : "media/songs/Try.mp3",
+        "songimage" : "media/Cil Album cover.jpg"
+    },
+    {
+        "title" : "Girl I used to be",
+        "artist" : "Cil",
+        "file"  : "media/songs/Girl I used to be.mp3",
+        "songimage" : "media/Cil Album cover.jpg"
+    },
+    {
+        "title" : "Devil in your eyes",
+        "artist" : "Cil",
+        "file"  : "media/songs/Devil In Your Eyes.mp3",
+        "songimage" : "media/Cil Album cover.jpg"
     },
 ]
 
@@ -30,9 +54,25 @@ function loadSong(){
 
     song.load();
     song.play();
-    
-
+    ctrlIcon.classList.remove("fa-play");
+    ctrlIcon.classList.add("fa-pause");
 }
+
+function prevSong(){
+    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;  // cycle through the entire list of songs eg if 0 is the current song index then it subtracts 0-1=-1 then adds the number of songs eg 3 -1=3=2 then divides by 3 again and rem is 2 so goes to song of index 2
+    loadSong();
+}
+
+function nextSong(){
+    currentSongIndex = (currentSongIndex + 1) % songs.length; // If currentSongIndex is at the last song (e.g. 2 when length is 3), adding 1 gives 3. 3 % 3 = 0 → goes back to the first song
+    loadSong();
+}
+
+song.onended = function (){
+    nextSong();  // Autoplay next song when current one ends
+};
+
+
 song.onloadedmetadata =function(){
     progress.max = song.oduration;
     progress.value = song.currentTime;
